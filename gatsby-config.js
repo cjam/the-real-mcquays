5ff7abf4776e213fe610700592e7ac784b7581be
Colter McQuay <colter.mcquay@gmail.com>
2019-06-19 11:03:08 -0700

    Upgraded gatsby and added typescript

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,33 +1,16 @@
-let contentfulConfig
-
-try {
-  // Load the Contentful config from the .contentful.json
-  contentfulConfig = require('./.contentful')
-} catch (_) {}
-
-// Overwrite the Contentful config with environment variables if they exist
-contentfulConfig = {
-  spaceId: process.env.CONTENTFUL_SPACE_ID || contentfulConfig.spaceId,
-  accessToken: process.env.CONTENTFUL_DELIVERY_TOKEN || contentfulConfig.accessToken,
-}
-
-const { spaceId, accessToken } = contentfulConfig
-
-if (!spaceId || !accessToken) {
-  throw new Error(
-    'Contentful spaceId and the delivery token need to be provided.'
-  )
-}
-
 module.exports = {
-  pathPrefix: '/gatsby-contentful-starter',
+  siteMetadata: {
+    siteName: `The Real McQuays`,
+  },
   plugins: [
-    'gatsby-transformer-remark',
-    'gatsby-plugin-react-helmet',
-    'gatsby-plugin-sharp',
+    `gatsby-plugin-typescript`,
+    `gatsby-transformer-remark`,
     {
-      resolve: 'gatsby-source-contentful',
-      options: contentfulConfig,
+      resolve: `gatsby-plugin-typography`,
+      options: {
+        pathToConfigModule: `src/utils/typography.js`,
+        omitGoogleFont: true,
+      },
     }
   ],
 }
